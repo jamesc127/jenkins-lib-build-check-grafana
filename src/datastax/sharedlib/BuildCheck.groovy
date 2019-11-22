b369547fd6efdb7f11d71fc15876b45af6ef048e
James Colvin <jamesc127@gmail.com>
2019-11-22 11:52:24 -0600

    adding echo for testing

--- src/datastax/sharedlib/BuildCheck.groovy
+++ src/datastax/sharedlib/BuildCheck.groovy
@@ -6,6 +6,7 @@ def runStDevCheck(startTime,endTime,baseline,tolerance,dseHost,grafanaHost) {
     echo 'does this work?'
     sh 'sudo chmod +x ./run-stdev-check.sh'
     sh "(./run-stdev-check.sh ${startTimeSeconds} ${endTimeSeconds} ${BUILD_ID} ${baseline} ${tolerance} ${dseHost} ${grafanaHost}) || true"
+    echo 'does this still work?'
 }
 
 def collectJUnit(){
