17203e170b782edcdaee1aaf76facdfb17c0571e
James Colvin <jamesc127@gmail.com>
2019-11-22 12:02:40 -0600

    testing without variables

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# jenkins-lib-template
+# jenkins-lib-build-check-grafana
 Template Repo for Jenkins shared library
 
 Created from Jenkins reference on [Shared Libraries](https://jenkins.io/doc/book/pipeline/shared-libraries/)
--- src/datastax/sharedlib/BuildCheck.groovy
+++ src/datastax/sharedlib/BuildCheck.groovy
@@ -1,11 +1,9 @@
 package datastax.sharedlib
 
 def runStDevCheck(startTime,endTime,baseline,tolerance,dseHost,grafanaHost) {
-    def startTimeSeconds = startTime.div(1000).intValue()
-    def endTimeSeconds = endTime.div(1000).intValue()
     echo 'does this work?'
     sh 'sudo chmod +x ./run-stdev-check.sh'
-    sh "(./run-stdev-check.sh ${startTimeSeconds} ${endTimeSeconds} ${BUILD_ID} ${baseline} ${tolerance} ${dseHost} ${grafanaHost}) || true"
+    sh "(./run-stdev-check.sh ${startTime} ${endTime} ${BUILD_ID} ${baseline} ${tolerance} ${dseHost} ${grafanaHost}) || true"
     echo 'does this still work?'
 }
 
