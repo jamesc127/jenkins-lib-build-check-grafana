4d7e03cf87e36bb5b187765c74960f4835a39147
James Colvin <jamesc127@gmail.com>
2019-11-22 11:39:05 -0600

    cutting down methods to just the actual step

--- src/datastax/sharedlib/BuildCheck.groovy
+++ src/datastax/sharedlib/BuildCheck.groovy
@@ -3,15 +3,12 @@ package datastax.sharedlib
 def runStDevCheck(startTime,endTime,baseline,tolerance,dseHost,grafanaHost) {
     def startTimeSeconds = startTime.div(1000).intValue()
     def endTimeSeconds = endTime.div(1000).intValue()
-    steps{
-        sh 'sudo chmod +x ./run-stdev-check.sh'
-        sh "(./run-stdev-check.sh ${startTimeSeconds} ${endTimeSeconds} ${BUILD_ID} ${baseline} ${tolerance} ${dseHost} ${grafanaHost}) || true"
-    }
-    post{
-        always{
-            junit './*.xml'
-        }
-    }
+    sh 'sudo chmod +x ./run-stdev-check.sh'
+    sh "(./run-stdev-check.sh ${startTimeSeconds} ${endTimeSeconds} ${BUILD_ID} ${baseline} ${tolerance} ${dseHost} ${grafanaHost}) || true"
+}
+
+def collectJUnit(){
+    junit './*.xml'
 }
 
 return this
