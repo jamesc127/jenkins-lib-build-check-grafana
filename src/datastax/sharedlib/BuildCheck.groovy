19186e68b744333ce9555d42166e6cf865e8f280
James Colvin <jamesc127@gmail.com>
2019-11-22 11:51:59 -0600

    adding echo for testing

--- src/datastax/sharedlib/BuildCheck.groovy
+++ src/datastax/sharedlib/BuildCheck.groovy
@@ -3,6 +3,7 @@ package datastax.sharedlib
 def runStDevCheck(startTime,endTime,baseline,tolerance,dseHost,grafanaHost) {
     def startTimeSeconds = startTime.div(1000).intValue()
     def endTimeSeconds = endTime.div(1000).intValue()
+    echo 'does this work?'
     sh 'sudo chmod +x ./run-stdev-check.sh'
     sh "(./run-stdev-check.sh ${startTimeSeconds} ${endTimeSeconds} ${BUILD_ID} ${baseline} ${tolerance} ${dseHost} ${grafanaHost}) || true"
 }
