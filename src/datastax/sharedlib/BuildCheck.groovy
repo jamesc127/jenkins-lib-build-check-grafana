97de072c1083ccb27428c4d6d58a626116a8c860
James Colvin <jamesc127@gmail.com>
2019-11-25 10:52:09 -0600

    relative file path for jar

--- src/datastax/sharedlib/BuildCheck.groovy
+++ src/datastax/sharedlib/BuildCheck.groovy
@@ -1,7 +1,7 @@
 package datastax.sharedlib
 
 def runStDevCheck(startTime,endTime,baseline,tolerance,dseHost,grafanaHost) {
-    sh "(java -Dconfig.file=./BuildCheck.json -Djenkins.xmlfilepath=. -Dgrafana.start-time=${startTime} -Dgrafana.end-time=${endTime} -Djenkins.build=${BUILD_ID} -Djenkins.baseline=${baseline} -Djenkins.stdevtolerance=${tolerance} -Ddse.host=${dseHost} -Dgrafana.host=${grafanaHost} -jar stdevcheck) || true"
+    sh "(java -Dconfig.file=./BuildCheck.json -Djenkins.xmlfilepath=. -Dgrafana.start-time=${startTime} -Dgrafana.end-time=${endTime} -Djenkins.build=${BUILD_ID} -Djenkins.baseline=${baseline} -Djenkins.stdevtolerance=${tolerance} -Ddse.host=${dseHost} -Dgrafana.host=${grafanaHost} -jar ./stdevcheck) || true"
 }
 
 def collectJUnit(){
