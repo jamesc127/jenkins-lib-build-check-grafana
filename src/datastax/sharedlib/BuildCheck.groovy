f43394b47e5db5ce2f65d6a5848067d68f30a54a
James Colvin <jamesc127@gmail.com>
2019-11-28 08:05:49 -0600

    adding grab for buildcheck repo

--- src/datastax/sharedlib/BuildCheck.groovy
+++ src/datastax/sharedlib/BuildCheck.groovy
@@ -1,11 +1,21 @@
 package datastax.sharedlib
 
+@GrabResolver(name = 'com.datastax', root = 'https://maven.pkg.github.com/jamesc127/jenkins-dse-build-compare', m2Compatible = true)
+@Grab(group = 'com.datastax', module = 'jenkins-dse-build-compare', version = '1.2')
+
+import com.datastax.BuildCompare
+
 def runStDevCheck(startTime,endTime,baseline,tolerance,dseHost,grafanaHost) {
     sh "(java -Dconfig.file=./BuildCheck.json -Djenkins.xmlfilepath=. -Dgrafana.start-time=${startTime} -Dgrafana.end-time=${endTime} -Djenkins.build=${BUILD_ID} -Djenkins.baseline=${baseline} -Djenkins.stdevtolerance=${tolerance} -Ddse.host=${dseHost} -Dgrafana.host=${grafanaHost} -jar ./stdevcheck) || true"
 }
 
 def collectJUnit(){
     junit './*.xml'
+}
+
+def runBuildCheck(startTime,endTime,baseline,tolerance,dseHost,grafanaHost){
+    String buildNumber = BUILD_ID
+    BuildCompare.runBuildCompare(startTime,endTime,baseline,tolerance,dseHost,grafanaHost,buildNumber)
 }
 
 return this
